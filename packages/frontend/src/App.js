935b1e11a2ea38cbcc77534c3ba11b3d6cfe9711
cyril cabrolier <cyr.cab@hotmail.fr>
2024-03-14 19:49:40 +0100

    test

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -2,6 +2,10 @@ export const App = () => {
   return (
     <div>
       <h1>Hello, world!</h1>
+      <h2>
+        This is a monorepo with a shared component library and a React
+        application.
+      </h2>
     </div>
   )
 }
